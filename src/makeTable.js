eeca6313f3789d0c1fd0d91e494e8168cdf6af9c
saniok017 <saniok017@gmail.com>
2019-03-05 18:27:48 +0300

    feat: - configure data for table

--- src/makeTable.js
+++ src/makeTable.js
@@ -1,6 +1,8 @@
 import { template } from 'lodash';
+import orders from '../data/orders'
 
 function makeTable() {
+    console.log(orders)
     return (template('<table>\
         <thead>\
             <tr>\
@@ -13,10 +15,20 @@ function makeTable() {
                 <th>Location</th>\
             </tr>\
         </thead>\
+        <% orders.forEach(function(order) { %> \
+            <tr id="order_<%-order.id%>"> \
+                <td><%-order.transaction_id%></td> \
+                <td class="user_data"><%-order.user_id%></td>\
+                <td>21/12/2017, 1:13:47 AM</td>\
+                <td>$<%-order.total%></td>\
+                <td><%-order.card_number%></td>\
+                <td><%-order.card_type%></td>\
+                <td>IS (<%-order.order_ip%>)<td>\
+            </tr>\
+        <% }); %> \
         <tbody>\
-            тут будут данные о заказах...\
         </tbody>\
-    </table>'))()
+    </table>'))({orders})
 }
 
 export default makeTable;
